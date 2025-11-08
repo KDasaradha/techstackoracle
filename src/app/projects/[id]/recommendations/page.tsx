2c6b21e6e59eedda0839127dba2a420962c8e884
KDasaradha <kdasaradha525@gmail.com>
2025-11-08 15:32:42 +0530

    fix: code fixes fo incorrect syntax

--- src/app/projects/[id]/recommendations/page.tsx
+++ src/app/projects/[id]/recommendations/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
